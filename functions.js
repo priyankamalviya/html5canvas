3080b0f30c3cf687383665d755835e57d4f5cab8
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-10 13:42:54 -0800

    strokes and fills

--- functions.js
+++ functions.js
@@ -2,5 +2,19 @@ window.onload = function(){
   //id
   var canvas = document.getElementById("canvas"),
       context = canvas.getContext("2d");
-      context.fillRect(10,10,100,200);
+      //context.fillRect(10,10,100,200);
+
+      context.beginPath();
+
+      //drawing commands
+
+      //context.fill();
+      context.moveTo(100,100);
+      context.lineTo(300,100);
+      context.lineTo(300,200);
+      context.lineTo(100,200);
+      //context.lineTo(100,100);
+
+      context.closePath();
+      context.stroke();
 }
