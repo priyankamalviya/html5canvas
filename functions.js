d95dfa2fa34395aa0a286a4c1978fa938b98a80c
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-20 22:33:45 -0800

    using globalCompositeOperation and adding canvas shadow effects

--- functions.js
+++ functions.js
@@ -1,21 +1,35 @@
 window.onload = function() {
    var canvas = document.getElementById("canvas"),
    context = canvas.getContext("2d");
-//   //image= document.getElementById("dog");
-//   image = document.createElement("img");
-//
-// image.src = "http://bit-101.com/dog.jpg";
-// image.addEventListener("load", function(){
-//   context.drawImage(image,200,10,150,150,0,0,300, 150)
-// });
 
-context.beginPath();
-for(var i=0;i<100;i++){
-  context.moveTo(Math.random() * 600, Math.random() * 600);
-  context.lineTo(Math.random() * 600, Math.random() * 600);
-}
-context.stroke();
-var image = document.createElement("img");
-image.src= canvas.toDataURL("image/jpg", 0.9);
-document.body.appendChild(image);
+   context.shadowColor = "rgba(0,0,0,0.5)";
+
+   //set shadow dynamically
+   document.body.addEventListener("mousemove", function(event){
+     context.clearRect(0,0,600,600);
+     var dx = 300- event.clientX,
+     dy = 300 - event.clientY,
+     distance = Math.sqrt(dx * dx + dy * dy);
+
+
+     context.shadowOffsetX = dx * 0.5;
+     context.shadowOffsetY = dy * 0.5;
+     context.shadowBlur = distance * 0.25;
+     context.fillStyle = "red";
+     context.fillRect(250, 250, 100, 100);
+   });
+
+   //set shadow manually
+  //  context.shadowBlur = 10;
+  //  context.shadowOffsetX = 10;
+  //  context.shadowOffsetY = 10;
+   //
+  //  context.fillStyle = "red",
+  //  context.fillRect(50,200,400,100);
+
+
+  //  context.globalAlpha = 0.5;
+  //  context.globalCompositeOperation = "destination-over";
+  //  context.filStyle = "red";
+  //  context.fillRect(200,50,100,400);
 };
