d81703d57fbc231ca4495b1ff259d46f4ac8dde1
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-20 23:04:27 -0800

    using transformations

--- functions.js
+++ functions.js
@@ -2,16 +2,30 @@ window.onload = function() {
    var canvas = document.getElementById("canvas"),
    context = canvas.getContext("2d");
 
+   var num = 20;
+   context.translate(300, 300);
+   for(var i = 0; i<num; i++){
+     context.rotate(Math.PI * 2/ num);
+     context.beginPath();
+     context.arc(100, 0, 10, 0, Math.PI * 2);
+     context.fill();
+   }
+
+  //  context.rotate(Math.PI / 4);
+  //  context.translate(200, 0);
+  //  context.fillRect(0, 0, 100, 100);
+
   //  context.translate(100,100);
   //  context.fillRect(0,0,100,100);
 
-   for(var j =0; j<10; j++){
-     context.save();
-   for(var i =0; i<10; i++){
-     context.fillRect(0,0,30,30);
-     context.translate(40,0);
-   }
-   context.restore();
-   context.translate(0,40);
- }
+// context.scale(2,2);
+//    for(var j =0; j<10; j++){
+//      context.save();
+//    for(var i =0; i<10; i++){
+//      context.fillRect(0,0,30,30);
+//      context.translate(40,0);
+//    }
+//    context.restore();
+//    context.translate(0,40);
+//  }
 };
