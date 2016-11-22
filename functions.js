7990b16d928d4b253013beb90a192f07b11dfcd9
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-21 17:57:48 -0800

    create a basic sketching canvas using html5 canvas

--- functions.js
+++ functions.js
@@ -1,10 +1,28 @@
 window.onload = function() {
    var canvas = document.getElementById("canvas"),
-   context = canvas.getContext("2d");
+   context = canvas.getContext("2d"),
+   rect = canvas.getBoundingClientRect(),
+   mouseX,
+   mouseY;
 
-canvas.addEventListener("click", function(event){
+   canvas.addEventListener("mousedown", onMouseDown);
+
+function onMouseDown(event){
+  mouseX = event.clientX - rect.left;
+  mouseY = event.clientY - rect.top;
+  canvas.addEventListener("mousemove", onMouseMove);
+  document.body.addEventListener("mouseup",onMouseUp);
+}
+function onMouseMove(event){
   context.beginPath();
-  context.arc(event.clientX, event.clientY, 10, 0, Math.PI * 2);
+  context.moveTo(mouseX, mouseY);
+  mouseX = event.clientX - rect.left;
+  mouseY = event.clientY - rect.top;
+  context.lineTo(mouseX, mouseY);
   context.stroke();
-});
+}
+function onMouseUp(event){
+  canvas.removeEventListener("mousemove", onMouseMove);
+  document.body.removeEventListener("mouseup", onMouseUp);
+}
 };
