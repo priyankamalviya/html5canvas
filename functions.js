b1259cbb03e8755457b20e2ece50161023cda2b6
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-21 19:43:59 -0800

    animation using setInterval method in canvas

--- functions.js
+++ functions.js
@@ -1,95 +1,17 @@
 window.onload = function() {
    var canvas = document.getElementById("canvas"),
    context = canvas.getContext("2d"),
-   x =100,
-   y = 100;
+   xpos = 0;
 
-   document.body.addEventListener("keydown", function(event){
-     switch(event.keyCode){
-       case 37: //left
-       x--;
-       draw();
-       break;
+   setInterval(function(){
+     draw(xpos);
+     xpos++;
+   }, 1000/30);
 
-       case 39: //right
-       x++;
-       draw();
-       break;
-     }
-   });
-
-
-   function draw(){
-     context.clearRect(0, 0, canvas.width, canvas.height);
+   function draw(x){
+     context.clearRect(0, 0, 600, 600);
      context.beginPath();
-     context.arc(x, y, 20, 0, Math.PI * 2);
+     context.arc(x, 300, 20, 0, Math.PI * 2);
      context.fill();
    }
-
-   draw();
-  //  rect = canvas.getBoundingClientRect(),
-  //  mouseX,
-  //  mouseY;
-
-//   btn = {
-//     x: 100,
-//     y:100,
-//     w:100,
-//     h:50,
-//     selected: false
-//   };
-//
-//   drawButton();
-//   canvas.addEventListener("click", function(event){
-//     var rect = canvas.getBoundingClientRect(),
-//     x = event.clientX - rect.left,
-//     y = event.clientY - rect.top;
-//
-//     if(x>= btn.x && x<=btn.x + btn.w &&
-//       y>=btn.y && y<=btn.y + btn.h){
-//         btn.selected = !btn.selected;
-//         drawButton();
-//     }
-//
-//   });
-//
-//   canvas.addEventListener("mousemove", function(event){
-//     var rect = canvas.getBoundingClientRect(),
-//     x = event.clientX - rect.left,
-//     y = event.clientY - rect.top;
-//
-//     if(x>= btn.x && x<=btn.x + btn.w &&
-//       y>=btn.y && y<=btn.y + btn.h){
-//         canvas.style.cursor = "pointer";
-//     }
-//     else
-//       canvas.style.cursor = "auto";
-//   });
-//
-// function drawButton(){
-//   context.fillStyle = btn.selected ? "red": "gray";
-//   context.fillRect(btn.x, btn.y, btn.w, btn.h);
-// }
-
-
-//    canvas.addEventListener("mousedown", onMouseDown);
-//
-// function onMouseDown(event){
-//   mouseX = event.clientX - rect.left;
-//   mouseY = event.clientY - rect.top;
-//   canvas.addEventListener("mousemove", onMouseMove);
-//   document.body.addEventListener("mouseup",onMouseUp);
-// }
-// function onMouseMove(event){
-//   context.beginPath();
-//   context.moveTo(mouseX, mouseY);
-//   mouseX = event.clientX - rect.left;
-//   mouseY = event.clientY - rect.top;
-//   context.lineTo(mouseX, mouseY);
-//   context.stroke();
-// }
-// function onMouseUp(event){
-//   canvas.removeEventListener("mousemove", onMouseMove);
-//   document.body.removeEventListener("mouseup", onMouseUp);
-// }
 };
