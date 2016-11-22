c47f42a9bffd677d53150279ddc9bb4a5359faea
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-21 19:06:59 -0800

    handling keyboard events

--- functions.js
+++ functions.js
@@ -1,49 +1,75 @@
 window.onload = function() {
    var canvas = document.getElementById("canvas"),
    context = canvas.getContext("2d"),
+   x =100,
+   y = 100;
+
+   document.body.addEventListener("keydown", function(event){
+     switch(event.keyCode){
+       case 37: //left
+       x--;
+       draw();
+       break;
+
+       case 39: //right
+       x++;
+       draw();
+       break;
+     }
+   });
+
+
+   function draw(){
+     context.clearRect(0, 0, canvas.width, canvas.height);
+     context.beginPath();
+     context.arc(x, y, 20, 0, Math.PI * 2);
+     context.fill();
+   }
+
+   draw();
   //  rect = canvas.getBoundingClientRect(),
   //  mouseX,
   //  mouseY;
 
-  btn = {
-    x: 100,
-    y:100,
-    w:100,
-    h:50,
-    selected: false
-  };
-
-  drawButton();
-  canvas.addEventListener("click", function(event){
-    var rect = canvas.getBoundingClientRect(),
-    x = event.clientX - rect.left,
-    y = event.clientY - rect.top;
-
-    if(x>= btn.x && x<=btn.x + btn.w &&
-      y>=btn.y && y<=btn.y + btn.h){
-        btn.selected = !btn.selected;
-        drawButton();
-    }
-
-  });
-
-  canvas.addEventListener("mousemove", function(event){
-    var rect = canvas.getBoundingClientRect(),
-    x = event.clientX - rect.left,
-    y = event.clientY - rect.top;
-
-    if(x>= btn.x && x<=btn.x + btn.w &&
-      y>=btn.y && y<=btn.y + btn.h){
-        canvas.style.cursor = "pointer";
-    }
-    else
-      canvas.style.cursor = "auto";
-  });
-
-function drawButton(){
-  context.fillStyle = btn.selected ? "red": "gray";
-  context.fillRect(btn.x, btn.y, btn.w, btn.h);
-}
+//   btn = {
+//     x: 100,
+//     y:100,
+//     w:100,
+//     h:50,
+//     selected: false
+//   };
+//
+//   drawButton();
+//   canvas.addEventListener("click", function(event){
+//     var rect = canvas.getBoundingClientRect(),
+//     x = event.clientX - rect.left,
+//     y = event.clientY - rect.top;
+//
+//     if(x>= btn.x && x<=btn.x + btn.w &&
+//       y>=btn.y && y<=btn.y + btn.h){
+//         btn.selected = !btn.selected;
+//         drawButton();
+//     }
+//
+//   });
+//
+//   canvas.addEventListener("mousemove", function(event){
+//     var rect = canvas.getBoundingClientRect(),
+//     x = event.clientX - rect.left,
+//     y = event.clientY - rect.top;
+//
+//     if(x>= btn.x && x<=btn.x + btn.w &&
+//       y>=btn.y && y<=btn.y + btn.h){
+//         canvas.style.cursor = "pointer";
+//     }
+//     else
+//       canvas.style.cursor = "auto";
+//   });
+//
+// function drawButton(){
+//   context.fillStyle = btn.selected ? "red": "gray";
+//   context.fillRect(btn.x, btn.y, btn.w, btn.h);
+// }
 
 
 //    canvas.addEventListener("mousedown", onMouseDown);
