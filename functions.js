e218d0a3029d6520faf33b5cee09dbed9797f1a6
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-21 20:33:11 -0800

    animation using requestAnimationFrame method

--- functions.js
+++ functions.js
@@ -3,15 +3,21 @@ window.onload = function() {
    context = canvas.getContext("2d"),
    xpos = 0;
 
-   setInterval(function(){
-     draw(xpos);
-     xpos++;
-   }, 1000/30);
+  //  setInterval(function(){
+  //    draw(xpos);
+  //    xpos++;
+  //  }, 1000/30);
 
-   function draw(x){
+  setInterval(function(){
+    requestAnimationFrame(draw);
+  }, 1000/40);
+
+ function draw(){
      context.clearRect(0, 0, 600, 600);
      context.beginPath();
-     context.arc(x, 300, 20, 0, Math.PI * 2);
+     context.arc(xpos, 300, 20, 0, Math.PI * 2);
      context.fill();
+     xpos++;
+     //requestAnimationFrame(draw);
    }
 };
